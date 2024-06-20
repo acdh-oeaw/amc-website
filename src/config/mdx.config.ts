a47ea5a28fb9995770dc54d6c8e12c504d868e38
Stefan Probst <stefanprobst@posteo.de>
2024-06-20 15:56:19 +0200

    chore: align with template

--- src/config/mdx.config.ts
+++ src/config/mdx.config.ts
@@ -12,6 +12,7 @@ import type { Options as TypographicOptions } from "retext-smartypants";
 
 import type { Locale } from "@/config/i18n.config";
 import { config as syntaxHighlighterConfig } from "@/config/syntax-highlighter.config";
+// import { withMdxFootnotes } from "@/lib/content/footnotes";
 import { withMdxTableOfContents, withTableOfContents } from "@/lib/content/table-of-contents";
 import { createI18n } from "@/lib/i18n";
 
@@ -49,6 +50,7 @@ export async function createConfig(locale: Locale) {
 			footnoteLabel: t("Mdx.Footnotes"),
 		},
 		rehypePlugins: [
+			// withMdxFootnotes,
 			withHeadingIds,
 			withTableOfContents,
 			withMdxTableOfContents,
