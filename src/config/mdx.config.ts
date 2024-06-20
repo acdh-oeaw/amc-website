617145fa8252c87dfa85bce418459dfb6109c476
Stefan Probst <stefanprobst@posteo.de>
2024-06-20 16:06:01 +0200

    perf: disable unused syntax highlighting

--- src/config/mdx.config.ts
+++ src/config/mdx.config.ts
@@ -1,7 +1,7 @@
 import { join } from "node:path";
 
 import type { CompileOptions } from "@mdx-js/mdx";
-import withSyntaxHighlighter from "@shikijs/rehype";
+// import withSyntaxHighlighter from "@shikijs/rehype";
 import withAssets from "rehype-mdx-import-media";
 import withHeadingIds from "rehype-slug";
 import withFrontmatter from "remark-frontmatter";
@@ -11,7 +11,7 @@ import withTypographicQuotes from "remark-smartypants";
 import type { Options as TypographicOptions } from "retext-smartypants";
 
 import type { Locale } from "@/config/i18n.config";
-import { config as syntaxHighlighterConfig } from "@/config/syntax-highlighter.config";
+// import { config as syntaxHighlighterConfig } from "@/config/syntax-highlighter.config";
 // import { withMdxFootnotes } from "@/lib/content/footnotes";
 import { withMdxTableOfContents, withTableOfContents } from "@/lib/content/table-of-contents";
 import { createI18n } from "@/lib/i18n";
@@ -54,7 +54,7 @@ export async function createConfig(locale: Locale) {
 			withHeadingIds,
 			withTableOfContents,
 			withMdxTableOfContents,
-			[withSyntaxHighlighter, syntaxHighlighterConfig],
+			// [withSyntaxHighlighter, syntaxHighlighterConfig],
 			withAssets,
 		],
 	};
